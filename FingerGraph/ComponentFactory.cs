5153d435d7b60ffdbba2b87f6e888be8168842aa
Stanislav.Sartasov <Stanislav.Sartasov@777be764-2f3d-11df-9924-27d7866ff452>
2010-04-01 08:06:30 +0000

    Теперь оно хотя бы компиляется!

--- FingerGraph/ComponentFactory.cs
+++ FingerGraph/ComponentFactory.cs
@@ -5,11 +5,13 @@ using System.Text;
 using FingerGraph.Database;
 using FingerGraph.Matcher;
 using FingerGraph.Scanner;
+using FingerGraph.GriauleMatcher;
+using FutronicDrv;
 
 namespace FingerGraph {
     class ComponentFactory {
-        public IMatcher CreateMatcher() { return null; }
+        public IMatcher CreateMatcher() { return new GriauleMatcher.GriauleMatcher(); }
         public IDatabase CreateDatabase() { return new FingerDatabase(); }
-        public IScanner CreateSensors() { return null; }
+        public IScanner CreateScanner() { return new Device(); }
     }
 }
--- FingerGraph/Matcher/IMatcher.cs
+++ FingerGraph/Matcher/IMatcher.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Drawing;
-using FingerGraphDB;
 
 namespace FingerGraph.Matcher {
     interface IMatcher {
