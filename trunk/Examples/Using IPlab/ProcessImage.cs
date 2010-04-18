24697bbd4eb81fdc26b816a76e46f97724a76438
revolver112 <revolver112@777be764-2f3d-11df-9924-27d7866ff452>
2010-04-18 19:04:47 +0000

    ImageProcessingLab + examples added

---
+++ trunk/Examples/Using IPlab/ProcessImage.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using AForge.Imaging.Filters;
+
+namespace ImageProcessing
+{
+    class ProcessImage
+    {
+
+        // Apply filter on the image
+        private static void ApplyFilter(IFilter filter, ref Bitmap image)
+        {
+            if (filter is IFilterInformation)
+            {
+                IFilterInformation filterInfo = (IFilterInformation)filter;
+                if (!filterInfo.FormatTransalations.ContainsKey(image.PixelFormat))
+                {
+                    if (filterInfo.FormatTransalations.ContainsKey(PixelFormat.Format24bppRgb))
+                    {
+                        MessageBox.Show("The selected image processing routine may be applied to color image only.",
+                                        "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        MessageBox.Show(
+                            "The selected image processing routine may be applied to grayscale or binary image only.\n\nUse grayscale (and threshold filter if required) before.",
+                            "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    return;
+                }
+            }
+            try
+            {
+                // apply filter to the image
+                image = filter.Apply(image);
+            }
+            catch
+            {
+                MessageBox.Show("Error occured applying selected filter to the image", "Error", MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+            }
+        }
+
+
+        public static Image Process(Image inputimage)
+        {
+            Bitmap bmpimage = new Bitmap(inputimage);
+            ApplyFilter(new GrayscaleBT709(), ref bmpimage);            //grayscaling
+            //ApplyFilter(new GaussianBlur(10, 7), ref bmpimage);             //bluring
+            ApplyFilter(new Threshold(128), ref bmpimage); //binarizing
+
+            return bmpimage;
+        }
+
+
+    }
+}
