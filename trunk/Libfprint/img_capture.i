d84b58b4d4f3567c087e6574d9dfa413b5816b3d
v.e.evard <v.e.evard@777be764-2f3d-11df-9924-27d7866ff452>
2010-04-20 19:23:12 +0000

    Обертка для libfprint. Она пока не работает. Но уже хотя бы ругается на кривость native-библиотеки, а не managed-кода. ;) На самом деле оборачивается здесь не libfprint, а программа на С, ее использующая. Но лучше хоть так, чем вовсе никак, да?

---
+++ trunk/Libfprint/img_capture.i
@@ -0,0 +1,5 @@
+%module img_capture
+%{
+        #include "img_capture.h"
+%}
+%include "img_capture.h"
---
+++ trunk/Libfprint/main.cs
@@ -0,0 +1,8 @@
+using System;
+
+public class test {
+	
+	static void Main() {
+		img_capture.capture();
+	}
+}
